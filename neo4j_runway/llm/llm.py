66523bffe515b420ab926ed6b2ef8e5d5431a089
Manjunath Janardhan <manjunath.shiva@gmail.com>
2024-06-26 19:00:45 +0530

    Add ollama support . This change no longer supports OpenAI

--- neo4j_runway/llm/llm.py
+++ neo4j_runway/llm/llm.py
@@ -15,18 +15,7 @@ from ..resources.prompts.prompts import system_prompts
 from ..resources.prompts.prompts import model_generation_rules
 
 MODEL_OPTIONS = [
-    "gpt-4o",
-    "gpt-4o-2024-05-13",
-    "gpt-4",
-    "gpt-3.5-turbo",
-    "gpt-4-0125-preview",
-    "gpt-4-turbo-preview",
-    "gpt-4-1106-preview",
-    "gpt-4-0613",
-    "gpt-4-32k",
-    "gpt-4-32k-0613",
-    "gpt-3.5-turbo-1106",
-    "gpt-3.5-turbo-0125",
+    "llama3:instruct",
 ]
 
 
@@ -54,7 +43,8 @@ class LLM:
                     open_ai_key
                     if open_ai_key is not None
                     else os.environ.get("OPENAI_API_KEY")
-                )
+                ),
+                base_url="http://localhost:11434/v1"
             )
         )
         self.model = model
